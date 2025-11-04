90125a62e437407f6eae736cf7fcb82022abe4db
Manoj Kulkarni <manojkulkarni0077@gmail.com>
2025-11-04 19:23:32 +0530

    Task 2: Added Kafka listener and application properties

---
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -0,0 +1,15 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-group")
+    public void listen(Transaction transaction) {
+        // Log the received transaction
+        System.out.println("Received transaction: " + transaction);
+        System.out.println("Transaction amount: " + transaction.getAmount());
+    }
+}
